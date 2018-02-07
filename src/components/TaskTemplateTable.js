8a9c2ab3a0a5d62ab4cc0f6515c53af3dbbe164f
letsget23 <letsget23@gmail.com>
2018-02-07 23:44:48 +0900

    refactor: taskTemplateTable 목록 구성 함수 추가

--- src/components/TaskTemplateTable.js
+++ src/components/TaskTemplateTable.js
@@ -6,6 +6,19 @@ import './TaskTemplateTable.scss'
 
 class TaskTemplateTable extends Component {
     render() {
+        const selectTaskTemplatesOn = (day_of_week) => {
+            return this.props.taskTemplates.filter((task_template) => {
+                return task_template[day_of_week]
+            }).sort((prev, next) => {
+                return (
+                    (prev[day_of_week + 'order'] === null ? 0 : prev[day_of_week + 'order']) - 
+                    (next[day_of_week + 'order'] === null ? 0 : next[day_of_week + 'order'])
+                )
+            }).map((task_template, index) => {
+                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
+            })
+        }
+
         return (
             <Table bordered>
                 <thead>
@@ -21,83 +34,13 @@ class TaskTemplateTable extends Component {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.monday_order === null ? 0 : a.monday_order) - (b.monday_order === null ? 0 : b.monday_order)
-                            }).filter((task_template) => {
-                                return task_template.monday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.tuesday_order === null ? 0 : a.tuesday_order) - (b.tuesday_order === null ? 0 : b.tuesday_order)
-                            }).filter((task_template) => {
-                                return task_template.tuesday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.wednesday_order === null ? 0 : a.wednesday_order) - (b.wednesday_order === null ? 0 : b.wednesday_order)
-                            }).filter((task_template) => {
-                                return task_template.wednesday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.thursday_order === null ? 0 : a.thursday_order) - (b.thursday_order === null ? 0 : b.thursday_order)
-                            }).filter((task_template) => {
-                                return task_template.thursday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.friday_order === null ? 0 : a.friday_order) - (b.friday_order === null ? 0 : b.friday_order)
-                            }).filter((task_template) => {
-                                return task_template.friday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.saturdayder === null ? 0 : a.saturday_order) - (b.saturdayder === null ? 0 : b.saturday_order)
-                            }).filter((task_template) => {
-                                return task_template.saturday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
-                        <td>
-                        {
-                            this.props.taskTemplates.sort((a, b) => {
-                                return (a.sunday_order === null ? 0 : a.sunday_order) - (b.sunday_order === null ? 0 : b.sunday_order)
-                            }).filter((task_template) => {
-                                return task_template.sunday
-                            }).map((task_template, index) => {
-                                return (<TaskTemplateItem key={index} TaskTemplate={task_template} />)
-                            })
-                        }
-                        </td>
+                        <td>{selectTaskTemplatesOn('monday')}</td>
+                        <td>{selectTaskTemplatesOn('tuesday')}</td>
+                        <td>{selectTaskTemplatesOn('wednesday')}</td>
+                        <td>{selectTaskTemplatesOn('thursday')}</td>
+                        <td>{selectTaskTemplatesOn('thursday')}</td>
+                        <td>{selectTaskTemplatesOn('saturday')}</td>
+                        <td>{selectTaskTemplatesOn('sunday')}</td>
                     </tr>
                 </tbody>
             </Table>
