3dfb788b57a7fc3bdfbba53a9a8781f91298f9b3
letsget23 <letsget23@gmail.com>
2018-02-19 17:53:01 +0900

    localStorage의 access_token을 이용하도록 수정

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
+import storage from 'helpers/storage';
 
 const URL = "http://127.0.0.1:5000";
 // const URL = "https://55x12biogf.execute-api.ap-northeast-2.amazonaws.com/staging";
+
+const access_token = storage.get('__PRTD_USER__')['access_token']
 
 const res = axios.create({
     baseURL: URL,
     timeout: 10000,
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Access-Token": access_token
     }
 });
 
